4c0d22cf64cd19eb48b6cd11e674e7e5a89078c9
Quadrivium <coder.quadrivium4@gmail.com>
2025-04-04 16:14:39 +0200

    api url

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
-export const baseUrl =  process.env.NODE_ENV === 'development'? "http://" + window.location.hostname +":5000": process.env.NODE_ENV === "production"? "https://goalapi-e96x.onrender.com": "";
+export const baseUrl =  process.env.NODE_ENV === 'development'? "http://" + window.location.hostname +":5000": process.env.NODE_ENV === "production"? process.env.REACT_APP_API_URL: "";
 export const protectedUrl = baseUrl + "/protected";
 export const assetsUrl = baseUrl + "/assets/users";
 export const dayInMilliseconds = 1000 * 60* 60 *24
